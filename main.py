65f14c38539ad52ea77f54a3aeb965f2f9075c55
Yehor Zvihunov <sckomoroh@gmail.com>
2025-04-14 00:34:02 +0300

    add map for models
    Signed-off-by: Yehor Zvihunov <sckomoroh@gmail.com>

--- main.py
+++ main.py
@@ -8,24 +8,47 @@ from common_tools import load_image
 from preproc_tools import preprocess_image, preprocess_yolo_cls_with_pil
 from postproc_tools import classification_process, detect_process, segment_process, pose_process
 
-# debug section
-model = onnx.load("models/yolov8n-cls.onnx")
-for node in model.graph.node[:10]:
-    print(node.op_type, node.name)
 
 # Infer section
-engine = OnnxEngine('models/yolov8n-cls.onnx')
-engine.read_names('models/labels-cls.txt')
+
+models_map = {
+    "detec":{
+        "model": "models/yolov8n.onnx",
+        "labels": "models/labels-dtc.txt",
+    },
+    "pose":{
+        "model": "models/yolov8n-pose.onnx",
+        "labels": "models/labels-pose.txt",
+    },
+    "segment":{
+        "model": "models/yolov8n-seg.onnx",
+        "labels": "models/labels-seg.txt",
+    },
+    "classification":{
+        "model": "models/yolov8n-cls.onnx",
+        "labels": "models/labels-cls.txt",
+    }
+}
+
+engine = OnnxEngine(models_map["detec"]["model"])
+engine.read_names(models_map["detec"]["labels"])
 engine.dump_inputs()
 engine.dump_outputs()
 
 img = load_image('data/input.jpeg')
-input = preprocess_image(img, (224, 224))
-# input = preprocess_yolo_cls_with_pil(img, (224, 224))
+
+# classification only (see the output of the dump inputs)
+# input = preprocess_image(img, (224, 224))
+
+input = preprocess_image(img, (640, 640))
 
 tensor = { "images" : input}
 
 output_batch = engine.infer(tensor)
 
-classification_process(output_batch, engine)
+# classification_process(batch=output_batch, engine=engine)
+# pose_process(batch=output_batch, orig_img=img, conf_thres=0.5)
+# segment_process(batch=output_batch, engine=engine, orig_img=img, conf_thres=0.5)
+detect_process(batch=output_batch, engine=engine, orig_img=img, conf_thres=0.5)
+
 pass
